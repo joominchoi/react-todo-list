38b90d21f5d130dc1fe19c97dc048ec9d0eeff8c
JooMin Choi <joominchoi23@gmail.com>
2023-06-28 14:00:27 +0100

    updated button text

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -23,7 +23,7 @@ export function NewTodoForm({ onSubmit }) {
           id="item"
         />
       </div>
-      <button className="btn">Add</button>
+      <button className="btn">Add Item</button>
     </form>
   )
 }
